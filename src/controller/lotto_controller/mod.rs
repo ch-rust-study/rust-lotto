20426ac158381f126eda32fb0ab7eb33f310505c
tigger <tigger@channel.io>
2024-05-16 11:35:46 +0900

    feat: 당첨번호를 받는 로직 추가

--- src/controller/lotto_controller/mod.rs
+++ src/controller/lotto_controller/mod.rs
@@ -1,13 +1,17 @@
-use crate::{model::lotto_issuer::LottoIssuer, view::lotto_input};
+use crate::{model::lotto_issuer::LottoIssuer, view::lotto_input::{self, get_bonus_number, get_winning_numbers}};
 
 pub struct LottoController {
   issuer: LottoIssuer,
+  winning_numbers: Option<Vec<i32>>,
+  bonus_number: Option<i32>,
 }
 
 impl LottoController {
   pub fn new(issuer: LottoIssuer) -> LottoController {
     LottoController {
-      issuer
+      issuer,
+      winning_numbers: None,
+      bonus_number: None,
     }
   }
 
@@ -17,6 +21,12 @@ impl LottoController {
     amount / 1000
   }
 
+  fn set_winning_numbers(&mut self) {
+    // TODO: 에러처리
+    self.winning_numbers = Some(get_winning_numbers());
+    self.bonus_number = Some(get_bonus_number());
+  }
+
   fn issue_lottos(&mut self) {
     let count = self.get_count();
     self.issuer.issue_lottos(count);
@@ -24,5 +34,6 @@ impl LottoController {
 
   pub fn run(&mut self) {
     self.issue_lottos();
+    self.set_winning_numbers();
   }
 }
--- src/view/lotto_input/mod.rs
+++ src/view/lotto_input/mod.rs
@@ -11,3 +11,38 @@ pub fn get_amount() -> u32 {
     }
 }
 }
+
+pub fn get_winning_numbers() -> Vec<i32> {
+  let input = prompt("당첨번호를 ','로 구분하여 입력해주세요");
+  let mut winning_numbers_buffer = Vec::<i32>::new();
+
+  for num_str in input.trim().split(",") {
+    match num_str.parse::<i32>() {
+      Ok(n) => winning_numbers_buffer.push(n),
+      Err(e) => {
+        println!("{}", e.to_string());
+        return get_winning_numbers();
+      }
+    }
+  }
+
+  if winning_numbers_buffer.len() != 6 {
+    println!("당첨 번호는 6개를 입력해주세요.");
+    return get_winning_numbers();
+  }
+
+
+  return winning_numbers_buffer;
+}
+
+pub fn get_bonus_number() -> i32 {
+  let input = prompt("보너스 번호를 입력해주세요");
+
+  match input.parse::<i32>() {
+    Ok(n) => n,
+    Err(e) => {
+      println!("{}", e.to_string());
+      return get_bonus_number();
+    }
+  }
+}
